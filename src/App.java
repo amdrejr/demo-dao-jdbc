fb91c7e64d2b46a54df31a81c2aa1cce55b022f0
André Jr <andrejr.mello@hotmail.com>
2024-01-24 20:25:15 -0300

    department class create

--- src/App.java
+++ src/App.java
@@ -1,5 +1,11 @@
+import model.entities.Department;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args)  {
+       
+        Department dp = new Department(1, "TI");
+
+        System.out.println(dp);
+
     }
 }
