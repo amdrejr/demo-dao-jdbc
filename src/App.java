a5946097d3cf23da3305c8f95952b25e19bf1411
André Jr <andrejr.mello@hotmail.com>
2024-01-24 21:01:04 -0300

    create seller class

--- src/App.java
+++ src/App.java
@@ -1,11 +1,17 @@
+import java.sql.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class App {
     public static void main(String[] args)  {
        
         Department dp = new Department(1, "TI");
 
+        Seller seller = new Seller(30, "Pedro", "pedro@email.com", new Date(0), 2000d, dp);
+
         System.out.println(dp);
+        System.out.println(seller);
 
     }
 }
